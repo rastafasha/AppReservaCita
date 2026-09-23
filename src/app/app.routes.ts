60bed96dca872523b5248d5191cf022eaa475723
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-09-23 09:10:25 -0400

    fixed routas1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,23 +4,26 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
    // 🚀 LA CORRECCIÓN CRÍTICA PARA EL LINK DE INSTAGRAM:
   // Le dice a Angular que cuando el paciente entre a la raíz '/', lo mande mágicamente a '/home'
-  { 
-    path: '', 
-    redirectTo: 'home', 
-    pathMatch: 'full' 
-  },
+//   { 
+//     path: '', 
+//     redirectTo: 'home', 
+//     pathMatch: 'full' 
+//   },
   
-  // Tu ruta original que renderiza las tarjetas estilo Apple
-  { 
-    path: 'home', 
-    component: HomeComponent 
-  },
+//   // Tu ruta original que renderiza las tarjetas estilo Apple
+//   { 
+//     path: 'home', 
+//     component: HomeComponent 
+//   },
 
-  // 🛡️ CORTAFUEGOS: Cualquier otra ruta rota o inexistente la mandamos también al Home
-  { 
-    path: '**', 
-    redirectTo: 'home' 
-  }
+//   // 🛡️ CORTAFUEGOS: Cualquier otra ruta rota o inexistente la mandamos también al Home
+//   { 
+//     path: '**', 
+//     redirectTo: 'home' 
+//   }
+
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
     
 
 ];
